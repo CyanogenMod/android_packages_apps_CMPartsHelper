c92509d1dd90264ca594a4c5afe1da504804ae3a
Jef Oliver <jgoliver@jeago.com>
2010-08-06 01:48:17 -0600

    Cleanup

--- src/com/cyanogenmod/cmpartshelper/mvsduiReceiver.java
+++ src/com/cyanogenmod/cmpartshelper/mvsduiReceiver.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 import java.io.IOException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.DataInputStream;
 
@@ -20,23 +21,19 @@ public class mvsduiReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(mvSdUi)) {
-            boolean success = true;
-            try {
-              FileInputStream infile = new FileInputStream(Environment.getExternalStorageDirectory() + "/cmparts_ui.xml");
-              DataInputStream in = new DataInputStream(infile);
-              byte[] b = new byte[in.available()];
-              in.readFully(b);
-              in.close();
-              String result = new String(b, 0, b.length);
-              Intent UiSd = new Intent("com.cyanogenmod.cmpartshelper.RESTORE_CMPARTS_UI");
-              UiSd.putExtra("xmldataret", result);
-              context.sendBroadcast(UiSd);
-            } catch (Exception e) {
-              e.printStackTrace();
-              success = false;
-            }
-            if (success) {
-                Toast.makeText(context, R.string.helper_complete, Toast.LENGTH_SHORT).show();
+            File xmlFile = new File(Environment.getExternalStorageDirectory() + "/cmparts_ui.xml");
+            if (xmlFile.exists()) {
+                try {
+                    FileInputStream infile = new FileInputStream(xmlFile);
+                    DataInputStream in = new DataInputStream(infile);
+                    byte[] b = new byte[in.available()];
+                    in.readFully(b);
+                    in.close();
+                    String result = new String(b, 0, b.length);
+                    Intent UiSd = new Intent("com.cyanogenmod.cmpartshelper.RESTORE_CMPARTS_UI");
+                    UiSd.putExtra("xmldataret", result);
+                    context.sendBroadcast(UiSd);
+                } catch (Exception e) { }
             }
         }
     }
